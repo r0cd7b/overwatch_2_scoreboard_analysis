d6eae00924da8258f2c9147005203b1f430de9ad
r0cd7b <tkdrbs1928@naver.com>
2023-11-06 20:54:07 +0900

    commit

--- contributions.py
+++ contributions.py
@@ -1,9 +1,22 @@
+# pyinstaller --onefile contributions.py
 import pandas as pd
 import joblib
 import sklearn
 
 while True:
     scoreboard = pd.read_csv('scoreboard.csv')
+    print('<Scoreboard>')
+    print(scoreboard)
+    death_max = joblib.load('max.joblib')
+    scoreboard.loc[
+        (scoreboard['E'] == 0) &
+        (scoreboard['A'] == 0) &
+        (scoreboard['D'] == 0) &
+        (scoreboard['DMG'] == 0) &
+        (scoreboard['H'] == 0) &
+        (scoreboard['MIT'] == 0),
+        'D'
+    ] = death_max
     contributions = joblib.load('scaler.joblib').transform(scoreboard)
     contributions[:, 2] = 1 - contributions[:, 2]
     contributions *= joblib.load('importances.joblib')
@@ -12,13 +25,11 @@ while True:
     contributions[tanks] *= 2 / contributions[tanks].sum()
     contributions[damages] *= 4 / contributions[damages].sum()
     contributions[supports] *= 4 / contributions[supports].sum()
-    print('<Contributions>')
+    print('\n<Contributions>')
     print(f'TEAM 1:', end='')
     for contribution in contributions[:5]:
         print(f' {contribution:.2f}', end='')
     print(f'\nTEAM 2:', end='')
     for contribution in contributions[5:]:
         print(f' {contribution:.2f}', end='')
-    print('\n\n<Scoreboard>')
-    print(scoreboard)
     input('\n> ')
