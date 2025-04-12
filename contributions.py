fc09ddc1b44a56378cd36e7719d366432381856a
r0cd7b <tkdrbs1928@naver.com>
2025-04-12 15:58:46 +0900

    commit

--- contributions.py
+++ contributions.py
@@ -15,7 +15,6 @@ try:
     averages = importances.sum(0) / len(importances)
     while True:
         input_ = input('> ')
-
         scoreboard = read_csv('scoreboard.csv')
         scoreboard.loc[
             (scoreboard['E'] == 0) &
@@ -32,12 +31,14 @@ try:
             players = len(scoreboard)
             contributions = (scoreboard * averages).sum(1)
             contributions *= players / contributions.sum()
-            contributions = sorted(zip(range(1, players + 1), contributions), key=itemgetter(1), reverse=True)
-            for index, contribution in contributions:
-                print(f'{index:02d} {contribution:.2f}')
+            contributions = {index: contribution for index, contribution in enumerate(contributions, 1)}
+            sorted_ = sorted(contributions.items(), key=itemgetter(1), reverse=True)
+            for ranking, (index, contribution) in enumerate(sorted_, 1):
+                contributions[index] = contribution, ranking
+            for index, (contribution, ranking) in contributions.items():
+                print(f'{index:02d}: ({contribution:.2f}, {ranking:02d})')
             print()
         else:
             break
-
 except Exception as e:
     input(e)
