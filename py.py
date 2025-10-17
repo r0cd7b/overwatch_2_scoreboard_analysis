b23b7a7f9f530274e39d92934da79bb9657f9e51
r0cd7b <tkdrbs1928@naver.com>
2025-10-17 22:04:23 +0900

    commit

--- py.py
+++
@@ -1,5 +0,0 @@
-columns = ['T', 'R', 'E1', 'E0', 'A1', 'A0', 'DE1', 'DE0', 'DA4', 'DA3', 'DA2', 'DA1', 'DA0',
-           'H4', 'H3', 'H2', 'H1', 'H0', 'M4', 'M3', 'M2', 'M1', 'M0']
-
-data_array = expanded_df[columns].to_numpy(dtype='uint8')
-data_array
